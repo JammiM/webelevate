46ad4df28ea28f9b1033fe4acef2cf3581cd3eff
jammiM <jammim19@gmail.com>
2017-11-07 01:55:15 +0000

    Outlining code to create a guess

---
+++ SSWD/Units/4/workshop/Ex1/NumberGenerator.class.php
@@ -0,0 +1,17 @@
+<?php
+
+  /**
+   *  Simply selects a random number between 1 and 6
+   */
+
+  class NumberGenerator
+  {
+
+    function makeAGuess()
+    {
+      return rand(1,6);
+    }
+
+  }//NumberGenerator
+
+?>
